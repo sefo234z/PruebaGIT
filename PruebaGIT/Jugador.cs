7446667ac28bf3e7707e19fc331bd809e0d21ccd
sefo234z <elsergifoix@gmail.com>
2025-09-23 12:35:14 +0200

    clases creadas

--- PruebaGIT/Jugador.cs
+++ PruebaGIT/Jugador.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace PruebaGIT
 {
-    internal class Jugador
-    {
+        public enum ePosicion
+        {
+            POR,
+            DEF,
+            CEN,
+            DEL
+        }
+        public class Jugador
+        {
+            private String nombre;
+            private ePosicion posicion;
+            private int dorsal;
+
+        public ePosicion Posicion
+        {
+            get { return posicion; }
+            set { posicion = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Dorsal
+        {
+            get { return dorsal; }
+            set { dorsal = value; }
+        }
+        public Jugador()
+        {
+
+        }
+
+        public Jugador(ePosicion posicion,string nombre ,int dorsal)
+        {
+             this.posicion = posicion;
+             this.nombre = nombre;
+             this.dorsal = dorsal;
+        }
+        public override string ToString()
+        {
+             return $"{nombre} con dorsal {dorsal} juega en la posicion {posicion}";
+        }
+        }
     }
-}
